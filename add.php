745311ce48e12d85558e2b962cc1400dbf0c0ea2
viper23 <htonnycarlos@gmail.com>
2022-08-23 23:35:46 +0200

    modify

--- add.php
+++ add.php
@@ -8,11 +8,44 @@
     <title>AJOUT EMPLOYE</title>
 </head>
 <body>
+
+    <?php
+        // verifier que le button Ajouter à été cliqué
+        if (isset($_POST['button']))
+
+        // recupeartion des valeurs des variables
+        extract($_POST);
+
+        // verifier que les champs ont été remplis
+        if (isset($nom) && isset($prenom) && $age){
+            
+            // connection à la base de donnee
+            include_once "connection.php";
+
+            // requete d'ajout dans la base de donnee
+            $queryAdd  = "INSERT INTO employe_table VALUES (NULL,'$nom','$prenom','$age')";
+            $requete_ajout = mysqli_query($connection_to_database, $queryAdd);
+
+            if($requete_ajout){
+                // si la requete a reussie , redirection sur la page d'accueil
+                header("Location:index.php");
+            }
+        }else
+            $error_message_missing_input = "Veuillez remplir tous les champs !";
+
+    ?>
+
     <div class="form">
         <a href="index.php" class="back_button"><img src="images/back.png" alt="button_retour">Retour</a>
         <h2>Ajouter un employé</h2>
-        <p class="message_error">Veuillez remplir tous les champs</p>
-        <form action="" method="post">
+        <p class="message_error">
+            <?php 
+                // si la variable error_message_missing_input existe , affichons sa value
+                if (isset($error_message_missing_input))
+                    echo $error_message_missing_input;
+            ?>
+        </p>
+        <form action="" method="POST">
             <label for="nom">Nom</label>
             <input type="text" name="nom"> 
             <label for="">Prenom</label>
--- modify.php
+++ modify.php
@@ -8,17 +8,49 @@
     <title>MODIFIER EMPLOYE</title>
 </head>
 <body>
+
+    <?php
+
+        // connection à la base de donnee
+        include_once "connection.php";
+        
+        // recuperation de l'id dans le lien
+        $recuperation_id = $_GET["id"];
+
+        // requete pour afficher les infos de l'employe
+        $querySelectorModifier  = "SELECT * FROM employe_table WHERE id = $recuperation_id";
+        $requete_modifier = mysqli_query($connection_to_database, $querySelectorModifier);
+        $row_in_database = mysqli_fetch_assoc($requete_modifier);
+
+
+        // verifier que le button Ajouter à été cliqué
+        if (isset($_POST['button']))
+
+        // recupeartion des valeurs des variables
+        extract($_POST);
+
+        // verifier que les champs ont été remplis
+        if (isset($nom) && isset($prenom) && $age){
+
+            if($requete_ajout){
+                // si la requete a reussie , redirection sur la page d'accueil
+                header("Location:index.php");
+            }
+        }else
+            $error_message_missing_input = "Veuillez remplir tous les champs !";
+    ?>
+
     <div class="form">
         <a href="index.php" class="back_button"><img src="images/back.png" alt="button_retour">Retour</a>
         <h2>Modifier un employé</h2>
         <p class="message_error">Veuillez remplir tous les champs</p>
-        <form action="" method="post">
+        <form action="" method="POST">
             <label for="nom">Nom</label>
-            <input type="text" name="nom">
+            <input type="text" name="nom" value="<?=$row_in_database["nom"]?>">
             <label for="">Prenom</label>
-            <input type="text" name="prenom">
+            <input type="text" name="prenom" value="<?=$row_in_database["prenom"]?>">
             <label for="">Age</label>
-            <input type="number" name="age">
+            <input type="number" name="age" value="<?=$row_in_database["age"]?>">
             <input type="submit" value="Modifier" name="button">
         </form>
     </div>
